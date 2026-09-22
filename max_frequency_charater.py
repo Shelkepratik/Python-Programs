c7bcd647adb85adefcb5ea278a5ddcb97a427de9
Pratik Shelke <pratikshelke9090@gmail.com>
2026-09-22 19:52:04 +0530

    add new problem

---
+++ max_frequency_charater.py
@@ -0,0 +1,15 @@
+text=input("Enter the string: ")
+frequency={}
+for ch in text:
+    if ch in frequency:
+        frequency[ch]+=1
+    else:
+        frequency[ch]=1
+max_char=""
+max_count=0
+for ch in frequency:
+    if frequency[ch]>max_count:
+        max_count=frequency[ch]
+        max_char=ch
+print("Maximum frequency character:",max_char)
+print("frequency:",max_count)
